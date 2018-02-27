d00882c70a2282e8f7b0d7f3b3665f8e9bb70f91
karamba228 <igoorderke@gmail.com>
2018-02-27 12:26:03 -0600

    added brick breaker game

---
+++ src/org/game/brickbreaker/BrickBreaker.java
@@ -0,0 +1,44 @@
+package org.game.brickbreaker;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+import org.game.Game;
+
+public class BrickBreaker extends Game
+{
+
+   
+    public void start() 
+    {
+        
+    }
+
+   
+    public void run() 
+    {
+        
+    }
+
+    public void end() 
+    {
+        
+    }
+
+    public void reset() 
+    {
+        
+    }
+
+    public JPanel getPanel() 
+    {
+        return null;
+    }
+
+    public void getPane(Graphics g) 
+    {
+        
+    }
+
+}
---
+++ src/org/game/brickbreaker/BrickBreakerPanel.java
@@ -0,0 +1,5 @@
+package org.game.brickbreaker;
+
+public class BrickBreakerPanel {
+
+}
